573bb276882fb06f1c32fd3eb846c56a300808a1
Lyosha Chebotarev <alexey.p.chebotarev@gmail.com>
2026-06-19 04:32:28 +0300

    update the example

--- testing.ts
+++ testing.ts
@@ -2,13 +2,24 @@ import { createAPI } from './index.ts';
 
 const torch = createAPI({
   defaultDType: 'float16',
-  defaultDevice: 'cuda',
+  defaultDevice: 'cpu',
   pythonPath: './testing-python/.venv/bin/python'
 });
 
 const a = await torch.ones([2, 5]);
 const b = await torch.full([2, 3], 4);
 const c = await torch.cat([a, b], -1); // hovering in your IDE shows Tensor<[2, 8], 'float16', 'cuda'>
-torch.cat([a, b], 0); // all tensors must have the same shape except for the dimension of concatention
+
+console.log(c)
+// tensor([[1., 1., 1., 1., 1., 4., 4., 4.],
+//         [1., 1., 1., 1., 1., 4., 4., 4.]])
+
+
+
+// Uncomment for type errors:
+
+// await torch.cat([a, b], 0); // in your editor: **all tensors must have the same shape except for the dimension of concatention**
+// const d = await torch.zeros([2, 5], undefined, 'cuda');
+// await torch.stack([d, a]); // in your editor: **all tensors must be on the same device**
 
 await torch.close();
